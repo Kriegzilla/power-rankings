87b2b3b70abc1c0917ffa9a728624a981fd0c905
Ian Campbell <ianwcampbell.contact@gmail.com>
2015-09-02 16:09:10 -0400

    added rspec color and game class

--- spec/lib/game_spec.rb
+++ spec/lib/game_spec.rb
@@ -4,27 +4,27 @@ require "pry"
 
 describe Game do
 
-  let (:evo_finals) do
-    name = "Adam Lindgren"
-    tag = "Armada"
-    User.new(name, tag)
-  end
-
-  describe "#initialization" do
-
-    it "creates a new user" do
-      expect(armada).to be_a(User)
-    end
-
-    it "reads the user's name" do
-      expect(armada.name).to eq("Adam Lindgren")
-    end
-
-    it "reads the user's tag" do
-      expect(armada.tag).to eq("Armada")
-    end
-
-  end
+  # let (:evo_finals) do
+  #   name = "Adam Lindgren"
+  #   tag = "Armada"
+  #   User.new(name, tag)
+  # end
+  #
+  # describe "#initialization" do
+  #
+  #   it "creates a new user" do
+  #     expect(armada).to be_a(User)
+  #   end
+  #
+  #   it "reads the user's name" do
+  #     expect(armada.name).to eq("Adam Lindgren")
+  #   end
+  #
+  #   it "reads the user's tag" do
+  #     expect(armada.tag).to eq("Armada")
+  #   end
+  #
+  # end
 
 
 end
